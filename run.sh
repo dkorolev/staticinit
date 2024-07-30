f209eb0cb02efbb1f61e875edfb6ab3a0a2dad87
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-07-30 14:11:28 +0200

    Added `$GOLDEN` because bash is hard.

--- run.sh
+++ run.sh
@@ -8,4 +8,14 @@ S=""
 for i in $(./.current_debug/main | sort) ; do S="$S $i" ; done
 
 echo "Result: ${S:1}"
-[ "${S:1}" == 'Bar() bar1=1 bar2=2 Foo() main()' ]
+
+GOLDEN='Bar() bar1=1 bar2=2 Foo() main()'
+
+if [ "${S:1}" == 'Bar() bar1=1 bar2=2 Foo() main()' ] ; then
+  echo "Test: PASSED"
+else
+  echo "Test: FAILED"
+  echo "ACTUAL: '${S:1}'"
+  echo "GOLDEN: '$GOLDEN'"
+  exit 1
+fi
